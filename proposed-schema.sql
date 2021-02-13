426e88460f0cd7a75e045b5ae1ec8bfa7d0a3fd8
Andy Sterkowitz <andy@andysterkowitz.com>
2021-02-12 19:11:29 -0800

    More proposed schema changes

--- proposed-schema.sql
+++ proposed-schema.sql
@@ -8,15 +8,33 @@ CREATE TABLE users (
     PRIMARY KEY(id);
 );
 
-CREATE TABLE flashcard_categories (
+CREATE TABLE categories (
     id uuid DEFAULT generate_uuid_v4();
     name varchar NOT NULL,
     created_at timestampz NOT NULL DEFAULT NOW(),
     PRIMARY KEY(id);
 );
 
-CREATE TABLE flashcards (
+CREATE TABLE subcategories (
     id uuid DEFAULT generate_uuid_v4();
+    category_id uuid REFERENCES categories(id) NOT NULL,
+    name varchar NOT NULL,
+    created_at timestampz NOT NULL DEFAULT NOW(),
+    PRIMARY KEY(id);
+);
+
+CREATE TABLE subcategory_users (
+    id uuid NOT NULL DEFAULT generate_uuid_v4(),
+    subcategory_id uuid REFERENCES subcategories(id) NOT NULL,
+    user_id uuid REFERENCES users (id) NOT NULL,
+    created_at timestampz NOT NULL DEFAULT NOW(),
+    assigned_by_user_id uuid REFERENCES user(id) NOT NULL,
+    PRIMARY KEY(id);
+);
+
+CREATE TABLE flashcards (
+    id uuid DEFAULT generate_uuid_v4(),
+    subcategory_id uuid REFERENCES subcategories(id) NOT NULL,
     question varchar NOT NULL,
     answer varchar NOT NULL,
     type varchar NOT NULL, -- thinking multiple choice vs. open-ended
@@ -26,7 +44,7 @@ CREATE TABLE flashcards (
     PRIMARY KEY(id);
 );
 
-CREATE_TABLE flashcard_views (
+CREATE TABLE flashcard_views (
     id uuid DEFAULT generate_uuid_v4(),
     user_id uuid REFERENCES users(id) NOT NULL,
     flashcard_id uuid REFERENCES flashcards(id) NOT NULL,

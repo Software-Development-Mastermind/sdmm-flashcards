46c8898d518f622bd786ebbaf1c75f99d73c6d2f
Andy Sterkowitz <andy@andysterkowitz.com>
2021-02-12 18:16:44 -0800

    Changes to proposed schema

--- proposed-schema.sql
+++ proposed-schema.sql
@@ -4,15 +4,32 @@ CREATE TABLE users (
     last_name varchar NOT NULL,
     email varchar NOT NULL,
     password varchar NOT NULL,
-    created_at timestampz NOT NULL DEFAULT NOW()
+    created_at timestampz NOT NULL DEFAULT NOW(),
     PRIMARY KEY(id);
 );
 
 CREATE TABLE flashcard_categories (
     id uuid DEFAULT generate_uuid_v4();
+    name varchar NOT NULL,
+    created_at timestampz NOT NULL DEFAULT NOW(),
+    PRIMARY KEY(id);
 );
 
 CREATE TABLE flashcards (
     id uuid DEFAULT generate_uuid_v4();
-    created_at timestampz NOT NULL DEFAULT NOW()
+    question varchar NOT NULL,
+    answer varchar NOT NULL,
+    type varchar NOT NULL, -- thinking multiple choice vs. open-ended
+    created_at timestampz NOT NULL DEFAULT NOW(),
+    PRIMARY KEY(id);
 );
+
+CREATE_TABLE flashcard_views (
+    id uuid DEFAULT generate_uuid_v4(),
+    user_id uuid REFERENCES users(id) NOT NULL,
+    flashcard_id uuid REFERENCES flashcards(id) NOT NULL,
+    result varchar NOT NULL,
+    ip_address varchar, -- MAY BE OVERKILL HERE lol
+    created_at timestampz NOT NULL DEFAULT NOW(),
+    PRIMARY KEY(id);
+)

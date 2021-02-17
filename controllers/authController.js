37fc786850df36232c6aaaf7525e26554d1dbb7c
Andy Sterkowitz <andy@andysterkowitz.com>
2021-02-16 17:04:49 -0800

    Implements HTTPS for server

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
+const { HTTPS } = process.env;
 
 exports.login = async (request, response) => {
   const { body } = request;
 
-  response.cookie('sessionid', 2, {secure: false});
+  response.cookie('sessionid', 2, { secure: HTTPS === true });
+  
   response.status(201).send(body);
 }
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const https = require('https');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const sequelize = require('./data/database');
 const path = require('path');
 const bodyParser = require("body-parser");
+
+require('dotenv').config();
+
+const { HTTPS, PORT } = process.env;
+console.log(HTTPS);
 
 const app = express();
 
@@ -20,7 +26,19 @@ app.use((req, res) => {
 sequelize.sync()
   .then(result => {
     console.log("Connected to Database");
-    app.listen(3000);
+    if (HTTPS === 'true') {
+      const fs = require('fs');
+      const key = fs.readFileSync('./key.pem');
+      const cert = fs.readFileSync('./cert.pem');
+      const server = https.createServer({key: key, cert: cert }, app);
+      // unable to connect to HTTPS on chrome?
+      // open Chrome
+      // navigate to chrome://flags/
+      // see https://superuser.com/questions/772762/how-can-i-disable-security-checks-for-localhost
+      server.listen(PORT || 3000, () => { console.log('listening on ' + PORT || 3000) });
+    } else {
+      app.listen(PORT || 3000);
+    }
   })
   .catch(err => {
     console.log(err);

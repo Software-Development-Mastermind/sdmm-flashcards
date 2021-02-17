dd47805b706423897e84cc430639dc06c6463fc5
Andy Sterkowitz <andy@andysterkowitz.com>
2021-02-16 17:45:16 -0800

    Updates database.js to use env variables

--- data/database.js
+++ data/database.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 
 const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
-const sequelize = new Sequelize('sdmm_flashcards', 'postgres', 'postgres', {
-  host: 'localhost',
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: 'postgres',
 })
 

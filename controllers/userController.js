b660532043b05a7049cc4805e00f3ed96c388810
froggy <chrisdetmering@gmail.com>
2021-03-30 10:01:11 -0700

    Fixed typos Fixed typos in userRoutes.js and userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,13 @@
 const bcrypt = require('bcryptjs');
 const UserQueries = require('../data/queries/userQueries');
 
-exports.adduser = (req, res, next) => {
+exports.addUser = (req, res, next) => {
   const user = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     email: req.body.email,
     password: req.body.password,
   }
-
 
 }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,11 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+console.log(userController);
+
 router.post('/add-user', userController.addUser);
 router.get('/user', userController.getUserByEmail);
 router.put('/edit-user', userController.editUser);
-router.delete('/delete-user', userController.deleteuser);
+router.delete('/delete-user', userController.deleteUser);
 
 module.exports = router;

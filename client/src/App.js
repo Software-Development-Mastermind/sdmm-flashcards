a0f38822815e65ed165acd31cdeb39bb05be9d2c
froggy <chrisdetmering@gmail.com>
2021-03-31 10:24:36 -0700

    Removed css code in App.css
    Added components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import logo from './logo.svg';
-import Button from "@material-ui/core/button"; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Button 
-        variant="contained"
-        color="secondary">Login</Button>
-    </div>
+     <div>Login</div>
   );
 }
 

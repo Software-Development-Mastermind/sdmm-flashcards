9d513c236be6f602db09e200ab2a66e32aa9194e
froggy <chrisdetmering@gmail.com>
2021-03-31 10:24:36 -0700

    Adds absolute imports in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import logo from './logo.svg';
-import Button from "@material-ui/core/button"; 
+import Login from 'components/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Button 
-        variant="contained"
-        color="secondary">Login</Button>
-    </div>
+     <Login />
   );
 }
 

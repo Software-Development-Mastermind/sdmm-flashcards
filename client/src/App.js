d7b51cc948b99bb5bb66f27ff368a4b71f3126a5
Trevor Mearns <spiral272@gmail.com>
2021-04-10 07:29:14 +0900

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Login from 'components/Login/Login';
+import HomePage from 'components/HomePage/HomePage';
+import Navbar from 'components/Navbar';
 import './App.css';
 
 function App() {
-  return (<>
-    <CssBaseline />
-    <Router>
-      <Route exact path="/">
-        <Login />
-      </Route>
-    </Router>
-  </>);
+  return (
+    <>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Route exact path='/'>
+          <Login />
+        </Route>
+        <Route exact path='/home' component={HomePage} />
+      </Router>
+    </>
+  );
 }
 
 export default App;

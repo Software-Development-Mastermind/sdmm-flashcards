454aac4c43d8a6d14ff7e71043ae065ffff1afe5
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-04-28 18:37:31 -0400

    Resolved merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "components/Login/Login";
+import HomePage from "components/HomePage/HomePage";
 import "./App.css";
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <HomePage />
       <Router>
         <Route exact path="/">
           <Login />

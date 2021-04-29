666377df3caf1dac4b996176eb5e747486dd07d0
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-04-29 16:31:21 -0400

    Homepage desktop design finished and mobile WIP

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,45 +1,127 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import "./HomePage.css";
-import SDMM_LOGO from "../../assets/SDMM_LOGO.jpeg";
-import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import TwitterIcon from "@material-ui/icons/Twitter";
+
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import blue from "@material-ui/core/colors/blue";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 
-const useStyles = makeStyles(() => ({
-  typography: {
-    fontFamily: "Poppins",
+import "./HomePage.css";
+import SDMM_LOGO from "../../assets/SDMM_LOGO.svg";
+import BACKGROUND_IMG from "../../assets/background_image.jpeg";
+
+/*
+REFERENCES:
+
+https://stackoverflow.com/questions/52484812/reactjs-material-ui-breakpoints
+https://blog.logrocket.com/3-ways-to-add-custom-fonts-to-your-material-ui-project/
+*/
+
+const useStyles = makeStyles((theme) => ({
+  section: {
+    position: "absolute",
+    right: 0,
+    width: "100%",
+    minHeight: "100vh",
+    padding: "4em 8em",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    background: "#054771",
+    transition: "0.5s",
+    zIndex: 2,
+    [theme.breakpoints.down("xs")]: {
+      position: "absolute",
+      height: "100vh",
+      width: "100vw",
+      padding: "1em",
+      alignContent: "center",
+      justifyContent: "center",
+    },
   },
-  header: {
+  overlay: {
     position: "absolute",
     top: 0,
     left: 0,
     width: "100%",
-    padding: "40px 100px",
-    zIndex: 1000,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
+    height: "100%",
+    backgroundImage: `url(${BACKGROUND_IMG})`,
+    mixBlendMode: "overlay",
+  },
+  h2: {
+    fontFamily: "Poppins",
+    fontWeight: 800,
+    fontSize: "5em",
+    lineHeight: "1em",
+    textTransform: "uppercase",
+    color: blue[50],
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "3em",
+    },
+  },
+  h3: {
+    fontFamily: "Poppins",
+    fontSize: "4em",
+    fontWeight: 700,
+    lineHeight: "1em",
+    textTransform: "uppercase",
+    color: blue[50],
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "2.4em",
+    },
+  },
+  p: {
+    fontFamily: "Poppins",
+    fontSize: "1.1em",
+    margin: "2em 0",
+    fontWeight: 400,
+    maxWidth: "50%",
+    color: blue[50],
+    [theme.breakpoints.down("md")]: {
+      maxWidth: "90%",
+    },
+  },
+  header: {
+    position: "absolute",
+    padding: "4em 8em",
+    top: 0,
+    left: 0,
   },
   social: {
     position: "absolute",
-    zIndex: 10,
-    bottom: "20px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#fff",
+    bottom: "4em",
+    cursor: "pointer",
+    [theme.breakpoints.down("xs")]: {
+      bottom: "2em",
+    },
+  },
+  socialColor: {
+    color: blue[50],
   },
   button: {
+    fontFamily: "Poppins",
     fontSize: "1em",
     background: "#000",
-    padding: "10px 30px",
+    padding: "1em 3em",
     fontWeight: "500",
     marginTop: "2.5em",
     letterSpacing: "0.25em",
+    [theme.breakpoints.down("xs")]: {
+      marginTop: "1.5em",
+    },
+  },
+  img: {
+    width: "20em",
+    height: "auto",
+    [theme.breakpoints.down("xs")]: {
+      width: "10em",
+      height: "auto",
+    },
   },
 }));
 
@@ -48,23 +130,20 @@ function HomePage() {
 
   return (
     <>
-      <section class="showcase">
+      <section className={classes.section}>
         <div className={classes.header}>
-          <Typography variant="h4" className={classes.typography}>
-            SDMM
-          </Typography>
-          <div class="toggle"></div>
+          <img src={SDMM_LOGO} alt="SDMM Logo" className={classes.img} />
         </div>
 
-        <div class="overlay"></div>
-        <div class="text">
-          <Typography variant="h2" className={classes.typography}>
+        <div className={classes.overlay}></div>
+        <div>
+          <Typography variant="h2" className={classes.h2}>
             Never
           </Typography>
-          <Typography variant="h3" className={classes.typography}>
+          <Typography variant="h3" className={classes.h3}>
             Stop Learning
           </Typography>
-          <p>
+          <p className={classes.p}>
             Flash Cards for serious learners. Lorem ipsum dolor sit amet
             consectetur adipisicing elit. Dolorem fugit nihil aut debitis magni
             nisi natus temporibus sapiente veniam quasi! Lorem ipsum dolor sit
@@ -83,36 +162,16 @@ function HomePage() {
 
         <div className={classes.social}>
           <IconButton>
-            <GitHubIcon color="secondary" />
+            <GitHubIcon className={classes.socialColor} />
           </IconButton>
           <IconButton>
-            <FacebookIcon color="secondary" />
+            <FacebookIcon className={classes.socialColor} />
           </IconButton>
           <IconButton>
-            <TwitterIcon color="secondary" />
+            <TwitterIcon className={classes.socialColor} />
           </IconButton>
         </div>
       </section>
-
-      <div class="menu">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">News</a>
-          </li>
-          <li>
-            <a href="#">Destination</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#054771",
+    },
+    secondary: {
+      main: "#6D6E72",
+    },
+  },
 
-const theme = createMuiTheme({ 
-  palette: { 
-    primary: { 
-      main: "#054771"
-    }, 
-    secondary: { 
-      main: "#6D6E72"
-    }
-  }
-})
+  breakpoints: {
+    values: { xs: 0, sm: 600, md: 960, lg: 1280, xl: 1920 },
+  },
 
+  typography: {
+    fontFamily: ["popins"],
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-       <App />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
